5399e5f4d1f7bc8c4800d87f4806f72ec2f74dac
Will <willh2011@live.com>
2020-06-12 00:13:37 -0500

    Added comic books controller

---
+++ ComicBookGallery/ComicBookGallery/Controllers/ComicBooksController.cs
@@ -0,0 +1,17 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+using System.Web.Mvc;
+
+namespace ComicBookGallery.Controllers
+{
+    public class ComicBooksController : Controller
+    {
+        // GET: ComicBooks
+        public ActionResult Index()
+        {
+            return View();
+        }
+    }
+}
